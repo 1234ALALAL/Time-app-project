24507415470147c8caae8756778c458078b156ef
ClaytonSmith314 <44728116+ClaytonSmith314@users.noreply.github.com>
2020-04-18 19:30:35 -0500

    we can see buttons now

--- src/screens/OptionMenu.java
+++ src/screens/OptionMenu.java
@@ -4,27 +4,60 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import main.Widget;
+import widgets.Button;
+import widgets.Text;
 
 public class OptionMenu extends Widget{
 	
 	private Color background = new Color(100,100,100);
 	
+	public static final int OPTION_HEIGHT = 50;
+	
 	
 	public OptionMenu(Widget parent) {
 		super(parent);
 		graphic.setBounds(0, 20, 350, parent.graphic.getHeight());
+		
+		addW(new OptionButton(1, "option 1",this), 0, 0);
+		addW(new OptionButton(2, "option 2",this), 0, 0);
+		addW(new OptionButton(3, "option 3",this), 0, 0);
+		addW(new OptionButton(4, "option 4",this), 0, 0);
 	}
 	
 	public void render(Graphics g) {
 		g.setColor(background);
-		g.fillRect(graphic.x,graphic.y,graphic.width,graphic.height);
+		g.fillRect(graphic.x, graphic.y, graphic.width, graphic.height);
+		renderSelf(g);
 	}
 
 	public void tick() {
-		
+		tickSelf();
 	}
 
 	public void close() {
+		
+	}
+	
+	private class OptionButton extends Button {
+				
+		public OptionButton(int index, String text, OptionMenu opt) {
+			super(
+					opt.graphic.x, 
+					opt.graphic.y + (index-1)*OptionMenu.OPTION_HEIGHT + 20, 
+					opt.graphic.width, 
+					OptionMenu.OPTION_HEIGHT,
+					new Text(
+							opt.graphic.x + 30, 
+							opt.graphic.y + index*OptionMenu.OPTION_HEIGHT, 
+							text, 
+							OptionMenu.OPTION_HEIGHT - 30, 
+							"arial black", 
+							new Color(255,165,0)
+							),
+					opt.background
+					);
+			
+		}
 		
 	}
 
--- src/widgets/Button.java
+++ src/widgets/Button.java
@@ -11,12 +11,25 @@ public class Button extends Widget{
 	
 	private Text text;
 	private Color background;
+	private Color shadow = new Color(255,255,255,30);
 	
 	private boolean hover = false;
 	private boolean clicked = false;
 	
 	public Button(int x, int y, int width, int height) {
 		graphic.setBounds(x, y, width, height);
+	}
+	public Button(int x, int y, int width, int height, Text text, Color background) {
+		graphic.setBounds(x, y, width, height);
+		this.text = text;
+		 this.background = background;
+	}
+	
+	public void setText(Text text) {
+		this.text = text;
+	}
+	public void setBackground(Color background) {
+		this.background = background;
 	}
 	
 	public void render(Graphics g) {
@@ -32,13 +45,16 @@ public class Button extends Widget{
 	}
 	
 	protected void renderClicked(Graphics g) {
-		
 	}
 	protected void renderHover(Graphics g) {
-		
+		renderNone(g);
+		g.setColor(shadow);
+		g.fillRect(graphic.x, graphic.y, graphic.width, graphic.height);
 	}
 	protected void renderNone(Graphics g) {
-		
+		g.setColor(background);
+		g.fillRect(graphic.x, graphic.y, graphic.width, graphic.height);
+		text.render(g);
 	}
 
 	@Override
